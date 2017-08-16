c6a2ed3bfbd395fc51ee0d031f2d41db942cffe3
Brandon Bayer <b@bayer.ws>
2017-08-16 17:49:09 -0400

    here we go! Just a logo for now

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bee Design Website
+
+https://bee.design
+
+```bash
+yarn
+yarn start
+yarn build
+now out
+```
+
+**Proudly powered by [Next.js](https://github.com/zeit/next.js), [React](https://facebook.github.io/react/), and [Tachyons CSS](http://tachyons.io)**
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default () => (
+  <div className="vh-100 flex flex-auto justify-center items-center">
+    <Head>
+      <title>Bee Design LLC - Custom software design and development</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="stylesheet" href="https://unpkg.com/tachyons@4.8.0/css/tachyons.min.css"/>
+    </Head>
+
+    <img src="/static/Twitter header.png" />
+  </div>
+)
